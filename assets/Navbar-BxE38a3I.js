6b2d21d771bb1fb3577cf727e97bc335323140d8
takshil bundela <takshilbundela@takshils-MacBook-Air.local>
2026-03-20 14:52:16 +0530

    Updates

--- assets/Navbar-BxE38a3I.js
+++ assets/Navbar-BxE38a3I.js
@@ -1,4 +1,4 @@
-import{j,r as bt}from"./index-CsncJ2Jn.js";import{g as St,S as yt}from"./ScrollTrigger-0lJgtTtq.js";/*!
+import{j,r as bt}from"./index-BzTxkyMY.js";import{g as St,S as yt}from"./ScrollTrigger-0lJgtTtq.js";/*!
  * SplitText 3.14.2
  * https://gsap.com
  *
